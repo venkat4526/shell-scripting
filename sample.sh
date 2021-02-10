37ddad9d03679c6739b7955b8d45ce4f1c26fc12
venka4526 <venkat4522@gmail.com>
2021-02-10 18:04:12 -0500

    Adding sample.sh

--- sample.sh
+++ sample.sh
@@ -1,1 +1,13 @@
-echo Hello World
+#echo welcome devops venkat
+#way1
+sample() {
+  echo hello from sample function
+  }
+
+way2
+function sample1() {
+   echo Hello from sample1 fuction
+}
+##Access the function
+sample
+sample1
